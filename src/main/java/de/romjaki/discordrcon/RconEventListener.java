2f15b0a6e39101fa5b7ba3f4c797b32e94c85404
romangraef <roman.graef@gmail.com>
2018-01-16 21:55:29 +0100

    Added permission requirement to remove regardless of selfInvite

--- src/main/java/de/romjaki/discordrcon/RconEventListener.java
+++ src/main/java/de/romjaki/discordrcon/RconEventListener.java
@@ -1,6 +1,8 @@
 package de.romjaki.discordrcon;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -13,22 +15,14 @@ public class RconEventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         super.onMessageReceived(event);
-        String authorID = event.getAuthor().getId();
         String content = event.getMessage().getContentRaw();
         String[] arr = content.toLowerCase().split("\\s+", 2);
         System.out.println(content);
         if (!(arr[0].equals("/adduser") || arr[0].equals("/removeuser"))) {
             return;
         }
-        if (!Config.admins.contains(authorID) && !Config.selfInvite) {
-            event.getChannel().sendMessage(
-                    new EmbedBuilder()
-                            .setColor(Color.RED)
-                            .setTitle("You are lacking permissions")
-                            .setThumbnail(event.getAuthor().getAvatarUrl())
-                            .setDescription("You need to be an admin in order to perform that command.")
-                            .build())
-                    .queue(msg -> msg.delete().queueAfter(10, SECONDS));
+        if (!Util.isUserAdmin(event.getAuthor()) && !Config.selfInvite) {
+            sendPermissionMessage(event.getChannel(), event.getAuthor());
             return;
         }
         if (arr.length < 2) {
@@ -45,9 +39,24 @@ public class RconEventListener extends ListenerAdapter {
             proccessMessage(event, "add", "added", arr[1]);
         }
         if (arr[0].equals("/removeuser")) {
+            if (!Util.isUserAdmin(event.getAuthor())) {
+                sendPermissionMessage(event.getChannel(), event.getAuthor());
+                return;
+            }
             proccessMessage(event, "remove", "removed", arr[1]);
         }
 
+    }
+
+    private void sendPermissionMessage(MessageChannel channel, User author) {
+        channel.sendMessage(
+                new EmbedBuilder()
+                        .setColor(Color.RED)
+                        .setTitle("You are lacking permissions")
+                        .setThumbnail(author.getAvatarUrl())
+                        .setDescription("You need to be an admin in order to perform that command.")
+                        .build())
+                .queue(msg -> msg.delete().queueAfter(10, SECONDS));
     }
 
     private void proccessMessage(MessageReceivedEvent event, String action, String actioned, String name) {
--- src/main/java/de/romjaki/discordrcon/Util.java
+++ src/main/java/de/romjaki/discordrcon/Util.java
@@ -1,10 +1,16 @@
 package de.romjaki.discordrcon;
 
+import net.dv8tion.jda.core.entities.User;
+
 import java.io.IOException;
 
 public class Util {
     private Util() {
 
+    }
+
+    public static boolean isUserAdmin(User user) {
+        return Config.admins.contains(user.getId());
     }
 
     public static void whitelistPlayer(String name) throws IOException {
